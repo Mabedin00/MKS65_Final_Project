6df8fadacd00d39b39c29afe033c1252b84be657
asultan00 <asultan00@stuy.edu>
2020-01-10 11:21:49 -0500

    changed play_song to remove_song in header, & restarted

--- library.c
+++ library.c
@@ -9,12 +9,3 @@ void print_queue(struct songQ* queue) {
      }
      printf("\n");
 }
-
-struct songQ
-{
-     char name[100];
-     struct song *next;
-} void print_queue(struct songQ *queue);
-struct songQ *add_song(struct songQ *queue, char *name);
-struct songQ *play_song(struct songQ *queue);
-struct songQ *clear_queue(struct songQ *list);
--- library.h
+++ library.h
@@ -4,7 +4,7 @@
 struct songQ { char name[100]; struct song* next; }
 void print_queue(struct songQ* queue);
 struct songQ* add_song(struct songQ* queue, char* name);
-struct songQ* play_song(struct songQ* queue);
+struct songQ* remove_song(struct songQ* queue, char* name);
 struct songQ* clear_queue(struct songQ* list);
 
 #endif
