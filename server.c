68452a7be7f182c341f7b13a47365f5e915c695c
Mabedin00 <mabedin00@stuy.edu>
2020-01-14 18:54:14 -0500

    added random songs, but execlp not working properly and prompts show up twice

--- server.c
+++ server.c
@@ -3,27 +3,58 @@
 #include "play_song.h"
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <time.h>
 
 int game_over = 0;
 int client_socket = 0;
 
 void sighandler() {
-  printf("Game over nerds\n");
-  execute("killall", "aplay");
-  game_over = 1;
+	printf("Game over nerds\n");
+	execute("killall", "aplay");
+	game_over = 1;
 }
 
 void sighandler_2() {
-  char buffer[BUFFER_SIZE];
-  printf("Ran sighandler2\n");
-  strcpy(buffer, "Sorry but game's over\n");
-  write(client_socket, buffer, sizeof(buffer));
+	char buffer[BUFFER_SIZE];
+	printf("Ran sighandler2\n");
+	strcpy(buffer, "Sorry but game's over\n");
+	write(client_socket, buffer, sizeof(buffer));
 }
 
+int random_int(int min, int max){
+    srand(time(0));
+    return min + rand() % (max+1 - min);
+    
+}
+
+
 char * random_song() {
-  return "songs/americanpie.wav";
+	
+    char dir_to_scan[256] = "songs";
+	char *  song_name = malloc(100);
+	song_name = "/songs"; 
+	DIR * dir = opendir(dir_to_scan);
+	struct dirent * direntry = readdir(dir);
+    int count_files = -2;
+    for ( ; direntry != NULL; direntry = readdir(dir)){
+        count_files++;
+        }
+    if (count_files < 0) {
+        printf("No songs in the directory\n");
+    }
+    rewinddir(dir);
+    
+    int random_file = random_int(3,5);
+    count_files = 0;
+    for (; count_files != random_file; direntry = readdir(dir), count_files++){
+     }
+    strcat(song_name, direntry->d_name);
+	//printf("%s \n", song_name);
+    //return "song/americanpie.wav";
+	return song_name;
 }
 
 
@@ -36,7 +67,7 @@ void subserver(int client_socket) {
     if (strcmp(buffer, "americanpie.wav") == 0) {
         break;
     }
-    
+
     strcpy(buffer, "WRONG, DOOFUS");
     write(client_socket, buffer, sizeof(buffer));
   }//end read loop
@@ -60,7 +91,7 @@ int server() {
   listen_socket = server_setup();
 
   clear();
-  int pids[number_connections];
+  int pids[number_connections]; 
 
   while (1) {
 
@@ -90,6 +121,7 @@ int server() {
     // dup2(mypipe[0], STDIN_FILENO);
     f = fork();
     if (!f) {
+		//printf("%s qwewqeqwe \n", random_song());
         execlp("aplay", "aplay", random_song(), NULL);
     }
     else {
@@ -99,7 +131,7 @@ int server() {
 	  sleep(1);
       }
 	printf("We got out of the loop\n");
-	
+
 	int i;
 	for (i = 0; i < number_connections; i++) {
 	  kill(pids[i], SIGHUP); // just used for communications
@@ -107,4 +139,3 @@ int server() {
     }
   }
 }
-
