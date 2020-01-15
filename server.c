c6bd3f1412630afc11a66e8426a72034e77eb9ca
Mabedin00 <mabedin00@stuy.edu>
2020-01-15 11:47:58 -0500

    fixed random song

--- server.c
+++ server.c
@@ -2,12 +2,22 @@
 #include "server.h"
 #include "play_song.h"
 #include <sys/types.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <time.h>
 
 int game_over = 0;
 int client_socket = 0;
+
+
+int random_int(int min, int max){
+    srand(time(0));
+    return min + rand() % (max+1 - min);
+
+}
 
 void sighandler() {
   printf("Game over nerds\n");
@@ -23,9 +33,32 @@ void sighandler_2() {
 }
 
 char * random_song() {
-  return "songs/americanpie.wav";
-}
 
+    char dir_to_scan[256] = "songs";
+	char *  song_name = malloc(100);
+	strcpy(song_name, "songs/");
+	DIR * dir = opendir(dir_to_scan);
+	struct dirent * direntry = readdir(dir);
+    int count_files = -2;
+    for ( ; direntry != NULL; direntry = readdir(dir)){
+        count_files++;
+        }
+
+    if (count_files < 0) {
+        printf("No songs in the directory\n");
+    }
+    rewinddir(dir);
+
+    int random_file = random_int(3,5);
+    count_files = 0;
+
+    for (; count_files != random_file; direntry = readdir(dir), count_files++){
+     }
+    strcat(song_name, direntry->d_name);
+    // return "songs/americanpie.wav";
+	return song_name;
+
+}
 
 void subserver(int client_socket) {
   char buffer[BUFFER_SIZE];
@@ -36,7 +69,7 @@ void subserver(int client_socket) {
     if (strcmp(buffer, "americanpie.wav") == 0) {
         break;
     }
-    
+
     strcpy(buffer, "WRONG, DOOFUS");
     write(client_socket, buffer, sizeof(buffer));
   }//end read loop
@@ -86,10 +119,13 @@ int server() {
 	}
 
     }
+
     // super_duper = dup(STDIN_FILENO);
     // dup2(mypipe[0], STDIN_FILENO);
     f = fork();
+
     if (!f) {
+        // printf("%s\n", random_song());
         execlp("aplay", "aplay", random_song(), NULL);
     }
     else {
@@ -99,7 +135,7 @@ int server() {
 	  sleep(1);
       }
 	printf("We got out of the loop\n");
-	
+
 	int i;
 	for (i = 0; i < number_connections; i++) {
 	  kill(pids[i], SIGHUP); // just used for communications
@@ -107,4 +143,3 @@ int server() {
     }
   }
 }
-
