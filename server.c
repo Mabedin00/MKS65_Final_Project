5a36a55de037fb463d7c684013ffa735617a17ed
Brian <bmoses00@stuy.edu>
2020-01-17 20:01:40 -0500

    Reordered functions to put server at top to make use in nano easier

--- server.c
+++ server.c
@@ -22,77 +22,6 @@
 int game_over = 0;
 int client_socket = 0;
 
-
-int random_int(int min, int max){
-    srand(time(0));
-    return min + rand() % (max+1 - min);
-
-}
-
-void sighandler() {
-  printf("Game over nerds\n");
-  execute("killall", "aplay");
-  game_over = 1;
-}
-
-void sighandler_2() {
-  char buffer[BUFFER_SIZE];
-  printf("Ran sighandler2\n");
-  strcpy(buffer, "Sorry but game's over\n");
-  write(client_socket, buffer, sizeof(buffer));
-}
-
-char * random_song() {
-
-    char dir_to_scan[256] = "songs";
-	char *  song_name = malloc(100);
-	strcpy(song_name, "songs/");
-	DIR * dir = opendir(dir_to_scan);
-	struct dirent * direntry = readdir(dir);
-    int count_files = -2;
-    for ( ; direntry != NULL; direntry = readdir(dir)){
-        count_files++;
-        }
-
-    if (count_files < 0) {
-        printf("No songs in the directory\n");
-    }
-    rewinddir(dir);
-
-    int random_file = random_int(3,5);
-    count_files = 0;
-
-    for (; count_files != random_file; direntry = readdir(dir), count_files++){
-     }
-    strcat(song_name, direntry->d_name);
-    // return "songs/americanpie.wav";
-	return song_name;
-
-}
-
-void subserver(int client_socket, char * song_to_be_played) {
-  char buffer[BUFFER_SIZE];
-
-  while (read(client_socket, buffer, sizeof(buffer))) {
-
-    // code that I added
-    if (!strcmp(buffer, song_to_be_played)) {
-        break;
-    }
-
-    strcpy(buffer, "WRONG, DOOFUS");
-    write(client_socket, buffer, sizeof(buffer));
-  }//end read loop
-
-  kill(getppid(), SIGSYS);
-  strcpy(buffer, "You won!");
-
-  write(client_socket, buffer, sizeof(buffer));
-  close(client_socket);
-  exit(0);
-}
-
-
 int server(char * num_songs) {
   char buffer[100];
   int number_connections = 2;
@@ -140,17 +69,88 @@ int server(char * num_songs) {
 		    if (game_over == 1) break;
 		    sleep(1);
 		}
-		
+
 	    printf("We got out of the loop\n");
 
 	    int i;
 	    for (i = 0; i < number_connections; i++) {
 	        kill(pids[i], SIGHUP); // just used for communications
         }
-        
+
     } // end server else
-    
+
     num_songs++;
   } // end of while current_song < num_songs loop
 } // end of server function
-// end of file
+
+
+void subserver(int client_socket, char * song_to_be_played) {
+  char buffer[BUFFER_SIZE];
+
+  while (read(client_socket, buffer, sizeof(buffer))) {
+
+    // code that I added
+    if (!strcmp(buffer, song_to_be_played)) {
+        break;
+    }
+
+    strcpy(buffer, "WRONG, DOOFUS");
+    write(client_socket, buffer, sizeof(buffer));
+  }//end read loop
+
+  kill(getppid(), SIGSYS);
+  strcpy(buffer, "You won!");
+
+  write(client_socket, buffer, sizeof(buffer));
+  close(client_socket);
+  exit(0);
+}
+
+
+
+int random_int(int min, int max){
+    srand(time(0));
+    return min + rand() % (max+1 - min);
+
+}
+
+void sighandler() {
+  printf("Game over nerds\n");
+  execute("killall", "aplay");
+  game_over = 1;
+}
+
+void sighandler_2() {
+  char buffer[BUFFER_SIZE];
+  printf("Ran sighandler2\n");
+  strcpy(buffer, "Sorry but game's over\n");
+  write(client_socket, buffer, sizeof(buffer));
+}
+
+char * random_song() {
+
+    char dir_to_scan[256] = "songs";
+	char *  song_name = malloc(100);
+	strcpy(song_name, "songs/");
+	DIR * dir = opendir(dir_to_scan);
+	struct dirent * direntry = readdir(dir);
+    int count_files = -2;
+    for ( ; direntry != NULL; direntry = readdir(dir)){
+        count_files++;
+        }
+
+    if (count_files < 0) {
+        printf("No songs in the directory\n");
+    }
+    rewinddir(dir);
+
+    int random_file = random_int(3,5);
+    count_files = 0;
+
+    for (; count_files != random_file; direntry = readdir(dir), count_files++){
+     }
+    strcat(song_name, direntry->d_name);
+    // return "songs/americanpie.wav";
+	return song_name;
+
+}
